41e4b82a079f2458ca59127072c816a5d222fa9a
Curus Keel <1304819+CurusKeel@users.noreply.github.com>
2019-08-11 16:34:03 -0500

    remove remant starter files, basic themeing
    Super pleased at how this is coming together

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,17 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Not much to see here</h1>
+    <p>This is still a work in progress. For quick previewing of what is there:</p>
+    <ul>
+      <li><Link to="/guide/core/prep">Core Guide</Link></li>
+      <li><Link to="/guide/visuals/prep">Visuals Guide</Link></li>
+    </ul>
   </Layout>
 )
 
