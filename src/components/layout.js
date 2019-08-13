287b41c7d837cb40c60793f12f304cd8ada3e344
Curus Keel <1304819+CurusKeel@users.noreply.github.com>
2019-08-12 23:41:43 -0500

    initial Bootstrap container

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,6 +9,11 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Navbar from "react-bootstrap/Navbar"
+
 import Header from "./header"
 import "./layout.css"
 import "./layout.sass"
@@ -28,23 +33,26 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
+  <div>
+    <Navbar expand="lg">
+      <Container>
+        <Header siteTitle={data.site.siteMetadata.title} />
+      </Container>
+    </Navbar> 
+    <Container>
+      <Row>
+        <Col><main>{children}</main></Col>
+      </Row>
+      <Row>
+        <Col>
+          <footer>
             © {new Date().getFullYear()} The Phoenix Flavour. All rights reserved.<br/>
             <FontAwesomeIcon icon={faBook} /> by Phoenix, <FontAwesomeIcon icon={faCode} /> by WilliamImm.
-        </footer>
-      </div>
-    </>
+          </footer>
+        </Col>
+      </Row>
+    </Container>
+  </div>
   )
 }
 
